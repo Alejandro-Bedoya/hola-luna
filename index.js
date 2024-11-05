0423ce57c2b2995d92c251ece5f5976f3bcf726b
Alejandro-Bedoya <jalejandrobedoyab@gmail.com>
2024-11-05 09:15:59 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -24,3 +24,35 @@ navLinks.forEach(link => {
         section.scrollIntoView({ behavior: 'smooth' }); // Realiza el scroll a la sección
     });
 });
+
+
+// SECCION FECHA
+const countdownDate = new Date("November 15, 2024 09:00:00").getTime();
+const message = document.getElementById("message");
+
+function updateCountdown() {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+    // Cálculo de días, horas, minutos y segundos
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = days;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
+
+    // Mostrar mensaje al llegar a cero
+    if (distance < 0) {
+        clearInterval(countdownInterval);
+        document.querySelector(".countdown-timer").style.display = "none";
+        message.style.display = "block";
+        message.textContent = "¡Felíz Cumpleaños Preciosa...! En un momento te darás cuentas de algo, ¡Un beso hermosha!";
+    }
+}
+
+const countdownInterval = setInterval(updateCountdown, 1000);
+
